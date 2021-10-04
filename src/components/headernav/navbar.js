d54edf7df9a4a0891e439b1844f97813dbea67eb
Toby English <tobyenglish132@gmail.com>
2021-10-04 15:29:15 -0700

    built out layout grid and navbar components

---
+++ src/components/headernav/navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="navbar">
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar
